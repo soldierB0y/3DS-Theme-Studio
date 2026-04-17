5c28387c0b202957a084b34e865cc96287b2d315
SoldierB0y <jebatistav14@gmail.com>
2026-04-17 09:49:51 -0400

    test

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/3DS-Theme-Studio/'
+  plugins: [react()]
 })
